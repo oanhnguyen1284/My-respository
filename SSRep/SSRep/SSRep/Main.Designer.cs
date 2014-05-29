ce63847b769734d2a9dd34671911b37db6304c25
oanhnguyen1284 <hoangoanh1284@yahoo.com>
2014-05-29 17:14:04 +0700

    SSRep
    Replace pattern for text

---
+++ SSRep/SSRep/SSRep/Main.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace SSRep
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.rtbLeft = new System.Windows.Forms.RichTextBox();
+            this.rtbRight = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 266);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(639, 75);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Controls.Add(this.btnClose);
+            this.panel4.Controls.Add(this.btnReplace);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(308, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(331, 75);
+            this.panel4.TabIndex = 2;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(235, 17);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(84, 43);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(42, 17);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(98, 43);
+            this.btnReplace.TabIndex = 0;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.rtbLeft);
+            this.splitContainer1.Panel1MinSize = 50;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.rtbRight);
+            this.splitContainer1.Panel2MinSize = 50;
+            this.splitContainer1.Size = new System.Drawing.Size(639, 266);
+            this.splitContainer1.SplitterDistance = 213;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // rtbLeft
+            // 
+            this.rtbLeft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbLeft.Location = new System.Drawing.Point(0, 0);
+            this.rtbLeft.Name = "rtbLeft";
+            this.rtbLeft.Size = new System.Drawing.Size(213, 266);
+            this.rtbLeft.TabIndex = 0;
+            this.rtbLeft.Text = "<BILL_NUM, nvarchar(10),>";
+            // 
+            // rtbRight
+            // 
+            this.rtbRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbRight.Location = new System.Drawing.Point(0, 0);
+            this.rtbRight.Name = "rtbRight";
+            this.rtbRight.Size = new System.Drawing.Size(422, 266);
+            this.rtbRight.TabIndex = 0;
+            this.rtbRight.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Main
+            // 
+            this.ClientSize = new System.Drawing.Size(639, 341);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel3);
+            this.Name = "Main";
+            this.Text = "SSRep";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox rtbLeft;
+        private System.Windows.Forms.RichTextBox rtbRight;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SSRep/SSRep/SSRep/Main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace SSRep
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private string ReplacingString(string s)
+        {
+            const string pattern = @"<\b(\w+),\s(\w+)\(\d+\),>";//replace for <RATE_TYPE, nvarchar(4),>
+            const string pattern1 = @"<\b(\w+),\s(\w+)\(\d+[,]\d+\),>";//replace for <SUBTOTAL_2, decimal(17,2),>
+            const string pattern2 = @"<\b(\w+),\s(\w+),>";// replace for <AUDIT_CREATED_DTM, datetime,>
+            string dest = Regex.Replace(s, pattern, "?");
+            dest = Regex.Replace(dest, pattern1, "?");
+            dest = Regex.Replace(dest, pattern2, "?");
+            return dest;
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            string s= ReplacingString(rtbLeft.Text);
+            rtbRight.Text = s;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rtbLeft.Text = string.Empty;
+            rtbRight.Text = string.Empty;
+        }
+     
+     
+    }
+}
