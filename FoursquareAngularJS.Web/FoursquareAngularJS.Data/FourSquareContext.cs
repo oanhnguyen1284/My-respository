d2c237c80f4d6a27bcdddfc3cc2b9eb03ea392dd
oanhnguyen1284 <hoangoanh1284@yahoo.com>
2014-07-07 08:43:50 +0700

    building the SAP for FoursquareAngularJs

---
+++ FoursquareAngularJS.Web/FoursquareAngularJS.Data/FourSquareContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoursquareAngularJS.Data.Entities;
+using FoursquareAngularJS.Data.Mappers;
+
+namespace FoursquareAngularJS.Data
+{
+    public class FourSquareContext :DbContext
+    {
+        public FourSquareContext() : base("DBConnection") {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<FourSquareContext, FourSquareContextMigrationConfiguration>());
+        }
+
+        public DbSet<BookmarkedPlace> BookmarkedPlaces { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new BookmarkedPlaceMapper());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FoursquareAngularJS.Web/FoursquareAngularJS.Web/app/services/placesExplorerService.js
@@ -0,0 +1,20 @@
+﻿var requestParms = {
+    clientID: "DO5JJHGXBODWHZUZ2W45T0S35PKJH3MCLC1SKF5U4X3VF4YA",
+    clientSecret: "GF0PDCNGEKSU2GI4ANGBGBKTEUU0G3E3QYPO5YWFXRV33GY5",
+    version: "20140707"
+};
+
+app.factory('placesExplorerService', function ($resource) {
+    var requestUri = "";
+    return $resource(requestUri, {
+        action: "",
+        client_id: "",
+        client_secret: "",
+        v: requestParms.version,
+        venuePhotos: '1',
+        callback: 'JSON_CALLBACK'
+    },
+    {
+        get: { method: 'JSONP' }
+    });
+});
